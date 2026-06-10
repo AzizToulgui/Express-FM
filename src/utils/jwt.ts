5acd0c188f88527d7ed2d09c89697e5668a749c9
Aziz Toulgui <aziztoulgui20@gmail.com>
2026-06-10 21:40:42 +0100

    feat: added jwt middleware

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,14 +1,14 @@
-import { SignJWT } from 'jose'
+import { jwtVerify, SignJWT } from 'jose'
 import { createSecretKey } from 'crypto'
 import env from '../../env.ts'
 
-export interface JWTPayload {
+export interface JwtPayload {
   id: string
   email: string
   username: string
 }
 
-export const generateToken = (payload: JWTPayload) => {
+export const generateToken = (payload: JwtPayload) => {
   const secret = env.JWT_SECRET
   const secretKey = createSecretKey(secret, 'utf-8')
 
@@ -17,4 +17,11 @@ export const generateToken = (payload: JWTPayload) => {
     .setIssuedAt()
     .setExpirationTime(env.JWT_EXPIRES_IN)
     .sign(secretKey)
+}
+
+export const verifyToken = async (token: string): Promise<JwtPayload> => {
+  const secretKey = createSecretKey(env.JWT_SECRET, 'utf-8')
+  const { payload } = await jwtVerify(token, secretKey)
+
+  return payload as unknown as JwtPayload
 }
